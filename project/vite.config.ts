6fcc49560c68c5d9ac7d71d17ea88727dd10fb2f
najibazharun <najwaazharun10@gmail.com>
2025-06-09 21:09:52 +0700

    fix: Correct vite config path for ES Modules

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import path from 'path'
-import { fileURLToPath, URL } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)))
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      }
-    }
-  }
 })
